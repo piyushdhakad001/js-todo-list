29b10f5eb0007c4661bd4088c31df7c46837113c
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-15 12:27:06 +0530

    Implement task creation functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+const addButton = document.querySelector(".add");
+const input = document.querySelector(".input");
+
+const tasksContainer = document.querySelector(".tasks-container");
+
+let tasks = [];
+
+  tasksContainer.innerHTML = "";
+
+ 
+
+
+addButton.addEventListener("click", () => {
+  // trim function for removing white space from beginning and end
+  const taskText = input.value.trim();
+  if (taskText === "") {
+    return;
+  }
+  tasks.push({
+    text: taskText,
+    completed: false,
+  });
+
+   tasks.forEach((taskObj, index) => {
+    // create task container----------
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task-element");
+
+    const task = document.createElement("p");
+    task.classList.add("task-paragraph");
+    task.textContent = taskObj.text;
+
+    taskElement.appendChild(task);
+
+    tasksContainer.appendChild(taskElement);
+  });
+
+
+  input.value = "";
+});
